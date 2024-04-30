4523f4383e28a8c31478a7a84ce2deab1cc26d73
mstrauss <matthias.strauss@proton.me>
2024-04-30 15:18:13 +0200

    added gitignore to remove unwanted files

---
+++ philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mstrauss <mstrauss@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/30 15:07:34 by mstrauss          #+#    #+#             */
+/*   Updated: 2024/04/30 15:11:26 by mstrauss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <pthread.h>
+# include <sys/time.h>
+
+/* ---------------------- timeval struct for reference ---------------------- */
+// struct	timeval
+// {
+// 	time_t tv_sec;       /* seconds since Jan. 1, 1970 */
+// 	suseconds_t tv_usec; /* and microseconds */
+// };
+#endif
